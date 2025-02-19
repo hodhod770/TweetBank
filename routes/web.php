0f3dcd9e30f19387de284444cf59d45fa89c68cb
hodhod770 <alhodhod.yemen@gmail.com>
2025-02-19 17:44:31 +0300

    s

--- routes/web.php
+++ routes/web.php
@@ -29,45 +29,45 @@ Route::get('/', function () {
 Route::get('/campaign/{id}', function ($id,Request $request) {
     // dd($request->ip());
     $tw = twt::where('hmlh_id',$id)->get();
+    $ip = $request->ip();
+    
+    // جلب بيانات الموقع الجغرافي من ip-api.com
+    $response = Http::get("http://ip-api.com/json/{$ip}?fields=status,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,mobile,proxy,hosting");
+    $data = $response->json();
 
-    if (!$request->hasCookie('visited')) {
-        $ip = $request->ip();
-    
-        // جلب بيانات الموقع الجغرافي من ip-api.com
-        $response = Http::get("http://ip-api.com/json/{$ip}?fields=status,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,mobile,proxy,hosting");
-        $data = $response->json();
-    
-        $tweetr = new VisitsRecords();
-        $tweetr->ip = $ip;
-        $tweetr->hmlh_id = $id;
-        if ($data['status'] === 'success') {
-            $tweetr->continent = $data['continent'] ?? null;
-            $tweetr->continent_code = $data['continentCode'] ?? null;
-            $tweetr->country = $data['country'] ?? null;
-            $tweetr->country_code = $data['countryCode'] ?? null;
-            $tweetr->region = $data['region'] ?? null;
-            $tweetr->region_name = $data['regionName'] ?? null;
-            $tweetr->city = $data['city'] ?? null;
-            $tweetr->district = $data['district'] ?? null;
-            $tweetr->zip = $data['zip'] ?? null;
-            $tweetr->lat = $data['lat'] ?? null;
-            $tweetr->lon = $data['lon'] ?? null;
-            $tweetr->timezone = $data['timezone'] ?? null;
-            $tweetr->offset = $data['offset'] ?? null;
-            $tweetr->currency = $data['currency'] ?? null;
-            $tweetr->isp = $data['isp'] ?? null;
-            $tweetr->org = $data['org'] ?? null;
-            $tweetr->as = $data['as'] ?? null;
-            $tweetr->asname = $data['asname'] ?? null;
-            $tweetr->mobile = $data['mobile'] ?? false;
-            $tweetr->proxy = $data['proxy'] ?? false;
-            $tweetr->hosting = $data['hosting'] ?? false;
-        }
-        $tweetr->save();
-    
-        // ضبط كعكة الزيارة لمنع التكرار لمدة 10 دقائق
-        Cookie::queue('visited', '1', 10);
+    $tweetr = new VisitsRecords();
+    $tweetr->ip = $ip;
+    $tweetr->hmlh_id = $id;
+    if ($data['status'] === 'success') {
+        $tweetr->continent = $data['continent'] ?? null;
+        $tweetr->continent_code = $data['continentCode'] ?? null;
+        $tweetr->country = $data['country'] ?? null;
+        $tweetr->country_code = $data['countryCode'] ?? null;
+        $tweetr->region = $data['region'] ?? null;
+        $tweetr->region_name = $data['regionName'] ?? null;
+        $tweetr->city = $data['city'] ?? null;
+        $tweetr->district = $data['district'] ?? null;
+        $tweetr->zip = $data['zip'] ?? null;
+        $tweetr->lat = $data['lat'] ?? null;
+        $tweetr->lon = $data['lon'] ?? null;
+        $tweetr->timezone = $data['timezone'] ?? null;
+        $tweetr->offset = $data['offset'] ?? null;
+        $tweetr->currency = $data['currency'] ?? null;
+        $tweetr->isp = $data['isp'] ?? null;
+        $tweetr->org = $data['org'] ?? null;
+        $tweetr->as = $data['as'] ?? null;
+        $tweetr->asname = $data['asname'] ?? null;
+        $tweetr->mobile = $data['mobile'] ?? false;
+        $tweetr->proxy = $data['proxy'] ?? false;
+        $tweetr->hosting = $data['hosting'] ?? false;
     }
+    $tweetr->save();
+    // if (!$request->hasCookie('visited')) {
+       
+    
+    //     // ضبط كعكة الزيارة لمنع التكرار لمدة 10 دقائق
+    //     Cookie::queue('visited', '1', 10);
+    // }
 
     return view('welcome',['tw'=>$tw]);
 })->name('campaign');
